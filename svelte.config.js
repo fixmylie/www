c6846cc76971ce36f379660ee4573eadb0a288d7
Roman Gurinovich @fixmylie <gurinovich.rv@gmail.com>
2022-02-27 21:11:13 +0700

    chore: add postcss

--- svelte.config.js
+++ svelte.config.js
@@ -5,16 +5,12 @@ const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: preprocess({ postcss: true }),
 	kit: {
 		adapter: adapter(),
 		paths: {
 			base: dev ? '' : '/www',
 		},
-		// If you are not using a .nojekyll file, change your appDir to something not starting with an underscore.
-		// For example, instead of '_app', use 'app_', 'internal', etc.
 		appDir: 'internal',
 	}
 };
