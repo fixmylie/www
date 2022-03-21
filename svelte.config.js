fdafd239919ce5002eff8fb9d93afcead92af44a
Roman Gurinovich @fixmylie <gurinovich.rv@gmail.com>
2022-03-21 20:13:17 +0700

    remove www path

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,11 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
-const dev = process.env.NODE_ENV === 'development';
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: preprocess({ postcss: true }),
 	kit: {
 		adapter: adapter(),
-		paths: {
-			base: dev ? '' : '/www',
-		},
 		appDir: 'internal',
     prerender: {
       default: true
